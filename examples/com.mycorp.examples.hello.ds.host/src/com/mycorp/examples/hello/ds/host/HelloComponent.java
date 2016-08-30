0db1fa4a9b5dd73d6dd25ce49322a20623a4d3b6
Alex Ivanov <amivanoff@gmail.com>
2016-08-30 22:37:00 +0300

    Refactor JaxRSProviders

--- examples/com.mycorp.examples.hello.ds.host/src/com/mycorp/examples/hello/ds/host/HelloComponent.java
+++ examples/com.mycorp.examples.hello.ds.host/src/com/mycorp/examples/hello/ds/host/HelloComponent.java
@@ -1,5 +1,11 @@
 package com.mycorp.examples.hello.ds.host;
 
+import java.io.IOException;
+import java.util.Dictionary;
+
+import org.osgi.service.cm.ConfigurationException;
+import org.osgi.service.cm.ManagedService;
+import org.osgi.service.component.ComponentContext;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
@@ -16,10 +22,16 @@ import com.mycorp.examples.hello.model.HelloMessage;
 		,"service.exported.configs=ecf.jaxrs.jersey.server"
 		,"ecf.jaxrs.jersey.server.urlContext=http://localhost:8080"
 		,"ecf.jaxrs.jersey.server.alias=/hello"
+		,"service.pid=com.mycorp.examples.hello.ds.host.HelloComponent"
 	}
 )
-public class HelloComponent implements IHello {
-	
+public class HelloComponent implements IHello, ManagedService {
+	private String id;
+    private String database;
+    private String user;
+    private String password;
+    private String create;
+    
 	public HelloComponent() {
 	}
 	
@@ -44,15 +56,26 @@ public class HelloComponent implements IHello {
 	
 	
 	@Activate
-	public void start() {
+	public void activate(ComponentContext context) throws IOException {
+		Dictionary<String, Object> properties = context.getProperties();
+		//properties.put("database.id", "wewe");
+        id = (String) properties.get("database.id");
+        database = (String) properties.get("database");
+        user = (String) properties.get("user");
+        password = (String) properties.get("password");
+        create = (String) properties.get("create");	
 		System.out.println("Hello service started");
 	}
 	@Deactivate
-	public void stop() {
+	public void deactivate(ComponentContext context) {
 		System.out.println("Hello service stopped");
 	}
 	@Modified
 	public void modify() {
 		System.out.println("Hello service modified");
+	}
+
+	public void updated(Dictionary<String, ?> properties) throws ConfigurationException {
+		System.out.println(properties);
 	}
 }
